ffaf37e53a1c6e1f4602a5c59a98dc3a2b1dfd99
Klappstuhl101 <92731033+Klappstuhl101@users.noreply.github.com>
2021-10-27 19:07:19 +0200

    Shader nearly ready for use

--- objects/button_management.gd
+++ objects/button_management.gd
@@ -16,52 +16,17 @@ func _init(initEntities, initOutput):
 	previous = entities.get(CONSTANTS.DEU)
 	
 	
-func connectButtons():
-	for entity in entities.values():
-		print(entity.name)
-		match entity.name:
-			CONSTANTS.BAW:
-				entity.mapButton.connect("toggled", self, "_on_BAW_press")
-			CONSTANTS.BAY:
-				entity.mapButton.connect("toggled", self, "_on_BAY_press")
-			CONSTANTS.BER:
-				entity.mapButton.connect("toggled", self, "_on_BER_press")
-			CONSTANTS.BRA:
-				entity.mapButton.connect("toggled", self, "_on_BRA_press")
-			CONSTANTS.BRE:
-				entity.mapButton.connect("toggled", self, "_on_BRE_press")
-			CONSTANTS.HAM:
-				entity.mapButton.connect("toggled", self, "_on_HAM_press")
-			CONSTANTS.HES:
-				entity.mapButton.connect("toggled", self, "_on_HES_press")
-			CONSTANTS.MVP:
-				entity.mapButton.connect("toggled", self, "_on_MVP_press")
-			CONSTANTS.NIE:
-				entity.mapButton.connect("toggled", self, "_on_NIE_press")
-			CONSTANTS.NRW:
-				entity.mapButton.connect("toggled", self, "_on_NRW_press")
-			CONSTANTS.RLP:
-				entity.mapButton.connect("toggled", self, "_on_RLP_press")
-			CONSTANTS.SAA:
-				entity.mapButton.connect("toggled", self, "_on_SAA_press")
-			CONSTANTS.SCN:
-				entity.mapButton.connect("toggled", self, "_on_SCN_press")
-			CONSTANTS.SCA:
-				entity.mapButton.connect("toggled", self, "_on_SCA_press")
-			CONSTANTS.SLH:
-				entity.mapButton.connect("toggled", self, "_on_SLH_press")
-			CONSTANTS.THU:
-				entity.mapButton.connect("toggled", self, "_on_THU_press")
-			CONSTANTS.DEU:
-				print("connected Deutschland")
-				entity.mapButton.connect("toggled", self, "_on_DEU_press")
+
+func _update():
+#	HIER DIE UPDATES FÜR STATS ETC. AUCH SHADER
+	pass
 
 func resetAll(exception = ""):
 	for entity in entities.values():
 		if (entity.name != exception):
 			entity.mapButton.pressed = false
-			if (entity.name == CONSTANTS.NIE):
-				entity.mapButton.material.set_shader_param("testcolor", Vector3(0.0,1.0,0.0))
+#			if (entity.name == CONSTANTS.NIE):
+#				entity.mapButton.material.set_shader_param("testcolor", Vector3(0.0,1.0,0.0))
 
 func showStats():
 #	print(active.name)
@@ -70,6 +35,7 @@ func showStats():
 	pass
 
 func activate():
+#	HIERHER DIE GANZEN SHADER EINSTELLUNGEN UND ÜBERGABE
 	showStats()
 
 func _on_BAW_press(toggle):
@@ -124,7 +90,8 @@ func _on_NIE_press(toggle):
 	if toggle:
 		active = entities.get(CONSTANTS.NIE)
 #		self.material.set_shader_param("testcolor", Color(1.0,0.0,0.0))
-		active.mapButton.material.set_shader_param("testcolor", Vector3(1.0,0.0,0.0))
+		active.mapButton.material.set_shader_param("vaccinated", 0.001)
+		active.mapButton.material.set_shader_param("infected", 0.001)
 		activate()
 	else:
 		active.mapButton.pressed = true
@@ -178,3 +145,40 @@ func _on_DEU_press(toggle):
 	else:
 		active.mapButton.pressed = true
 
+func connectButtons():
+	for entity in entities.values():
+		match entity.name:
+			CONSTANTS.BAW:
+				entity.mapButton.connect("toggled", self, "_on_BAW_press")
+			CONSTANTS.BAY:
+				entity.mapButton.connect("toggled", self, "_on_BAY_press")
+			CONSTANTS.BER:
+				entity.mapButton.connect("toggled", self, "_on_BER_press")
+			CONSTANTS.BRA:
+				entity.mapButton.connect("toggled", self, "_on_BRA_press")
+			CONSTANTS.BRE:
+				entity.mapButton.connect("toggled", self, "_on_BRE_press")
+			CONSTANTS.HAM:
+				entity.mapButton.connect("toggled", self, "_on_HAM_press")
+			CONSTANTS.HES:
+				entity.mapButton.connect("toggled", self, "_on_HES_press")
+			CONSTANTS.MVP:
+				entity.mapButton.connect("toggled", self, "_on_MVP_press")
+			CONSTANTS.NIE:
+				entity.mapButton.connect("toggled", self, "_on_NIE_press")
+			CONSTANTS.NRW:
+				entity.mapButton.connect("toggled", self, "_on_NRW_press")
+			CONSTANTS.RLP:
+				entity.mapButton.connect("toggled", self, "_on_RLP_press")
+			CONSTANTS.SAA:
+				entity.mapButton.connect("toggled", self, "_on_SAA_press")
+			CONSTANTS.SCN:
+				entity.mapButton.connect("toggled", self, "_on_SCN_press")
+			CONSTANTS.SCA:
+				entity.mapButton.connect("toggled", self, "_on_SCA_press")
+			CONSTANTS.SLH:
+				entity.mapButton.connect("toggled", self, "_on_SLH_press")
+			CONSTANTS.THU:
+				entity.mapButton.connect("toggled", self, "_on_THU_press")
+			CONSTANTS.DEU:
+				entity.mapButton.connect("toggled", self, "_on_DEU_press")
